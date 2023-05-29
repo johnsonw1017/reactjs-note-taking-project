f27f3636ad142089693587d5a8d9512c5a45ac6e
Johnson Wang <106476588+johnsonw1017@users.noreply.github.com>
2023-05-29 20:45:17 +1000

    feat: create routes with placeholder text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Routes>
+          <Route path="/" element={<h1>TODO</h1>}/>
+          <Route path="/notes" element={<h1>TODO</h1>}/>
+          <Route path="/notes/:noteID" element={<h1>TODO</h1>}/>
+          <Route path="/notes/:noteID/edit" element={<h1>TODO</h1>}/>
+          <Route path="/notes/searcgByWord/:word" element={<h1>TODO</h1>}/>
+          <Route path="/notes/sort/duedate" element={<h1>TODO</h1>}/>
+          <Route path="/notes/sort/createddate" element={<h1>TODO</h1>}/>
+          <Route path="/notes/filter/overdue" element={<h1>TODO</h1>}/>
+          <Route path="/notes/filter/done" element={<h1>TODO</h1>}/>
+        </Routes>
     </div>
   );
 }
