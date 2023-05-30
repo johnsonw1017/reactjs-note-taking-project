88545593229540b3ca4c04d1ac2b4a8315c6a293
Johnson Wang <106476588+johnsonw1017@users.noreply.github.com>
2023-05-30 11:14:09 +1000

    feat: create reducer with placeholder instructions

--- src/contexts/NotesContext.jsx
+++ src/contexts/NotesContext.jsx
@@ -8,6 +8,30 @@ const initialNotesData = [
         isCompleted: false,
         dueDate: new Date.setDate(new Date().getDate() + 1), // Current time but one day in the future
         createdAtDate: Date.now()
-
     }
 ]
+
+const notesReducer = (previousState, instructions) => {
+    let stateEditable = [...previousState];
+
+    switch (instructions.type) {
+        case "create":
+            console.log("TODO: Create note and add to state");
+            break;
+        case "update":
+            console.log("TODO: Update specific note and overwrite it in state");
+            break;
+        case "sortByDueDate":
+            console.log("TODO: Sorted state data by due date");
+            break;
+        case "sortByCreatedAtDate":
+            console.log("TODO: Sorted bt created at date");
+            break;
+        case "sortById":
+            console.log("TODO: Sorted by ID, this is the default order");
+            break;
+        default:
+            console.log("Invalid instruction type provided, it was: " + instructions.type);
+            return previousState
+    }
+}
