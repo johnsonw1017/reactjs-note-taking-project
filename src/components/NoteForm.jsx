37c8c9a8ccbd403bf62cd09055851f2304e41a73
Johnson Wang <106476588+johnsonw1017@users.noreply.github.com>
2023-06-01 14:34:15 +1000

    feat:created NofteForm component, not rendered yet

---
+++ src/components/NoteForm.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+export default function NoteForm(prop){
+
+    const [localTitle, setLocalTitle] = useState("");
+    const [localDescription, setLocalDescription] = useState("");
+    const [localIsCompleted, setLocalIsCompleted] = useState("");
+    const [localDueDate, setLocalDueDate] = useState(new Date().setDate(new Date().getDate() + 1));
+    
+
+    return(
+        <div>
+            <form>
+            <label>Title:</label>
+            <input type="text" name="title" value={localTitle} onChange={setLocalTitle}/>
+            <label>Description:</label>
+            <input type="text" name="description" value={localDescription} onChange={setLocalDescription}/>
+            <label>Is Completed:</label>
+            <input type="text" name="title" value={localIsCompleted} onChange={setLocalIsCompleted}/>
+            <label>Due Date:</label>
+            <input type="text" name="dueDate" value={localDueDate} onChange={setLocalDueDate}/>
+            </form>
+        <button>Save Note</button>
+        </div>
+    )
+}
